d1e9a55401085f768bf6c7af45f54a3e6bf64dc7
Igor <iricigor@gmail.com>
2025-02-09 13:21:02 +0000

    move function into module

--- HelloWorld.fs
+++ HelloWorld.fs
@@ -4,21 +4,7 @@ module HelloWorld
 open System
 open System.Net.Http
 open System.Threading.Tasks
-
-// Function to fetch web page content
-let fetchWebPageContent (url: string) : Task<string> =
-    task {
-        use httpClient = new HttpClient()
-        let! response = httpClient.GetAsync(url)
-
-        // Check if the request was successful
-        if response.IsSuccessStatusCode then
-            let! content = response.Content.ReadAsStringAsync()
-            return content
-        else
-            // Return an error message with the status code
-            return $"Error: Failed to fetch content. Status code: {response.StatusCode} ({int response.StatusCode})"
-    }
+open FetchWebPageContent
 
 [<EntryPoint>]
 let main argv =
